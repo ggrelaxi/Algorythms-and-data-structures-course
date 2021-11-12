66e99d5293b3d22d3e26be406909f1c745b86c5a
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-11-12 17:22:48 +0300

    fix list

--- linked_list.py
+++ linked_list.py
@@ -9,15 +9,14 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.count = 0
+        self.length = 0
 
     def add_in_tail(self, item):
         if self.head is None:
-            self.head = item
+            self.head = self.tail = item
         else:
-            self.tail.next = item
-        self.tail = item
-        self.count += 1
+            self.tail.next = self.tail = item
+        self.length += 1
 
     def print_all_nodes(self):
         node = self.head
@@ -51,90 +50,138 @@ class LinkedList:
         return result
 
     def delete(self, val, all=False):
-        if all == False:
-            if self.head == None:
-                return
-            elif self.head != None and self.head == self.tail:
+        if self.head == None:
+            return
+        if self.head.next == None:
+            if self.head.value == val:     
                 self.head = None
                 self.tail = None
-                self.count = 0
-            elif self.head != self.tail:
-                node = self.head
-                nextNode = node.next
+                self.length = 0
+            return
 
-                while nextNode != None:
-                    if node.value == val and node == self.head:
-                        self.head = nextNode
-                        if node.next == self.tail:
-                            self.tail = nextNode
-                        return
-                    elif nextNode.value == val and node == self.tail:
-                        self.tail = node
-                        return
-                    elif nextNode.value == val and node != self.tail:
-                        node.next = nextNode.next
-                        return
-                    node = nextNode
-                    nextNode = nextNode.next
-        else:
-            if self.head == None:
+        node = self.head
+        nextNode = node.next
+
+        while nextNode != None:
+            if node.value == val and node == self.head:
+                self.head = nextNode
+                if node.next == self.tail:
+                    self.tail = nextNode
+                    self.length -= 1
                 return
+            elif nextNode.value == val and node == self.tail:
+                self.tail = node
+                self.length -= 1
+                return
+            elif nextNode.value == val and node != self.tail:
+                node.next = nextNode.next
+                self.length -= 1
+                return
+            node = nextNode
+            nextNode = nextNode.next
 
-            prevNode = None
-            node = self.head
+            if all == False:
+                break
+        # if all == False:
+        #     if self.head == None:
+        #         return
+        #     elif self.head != None and self.head == self.tail:
+        #         self.head = None
+        #         self.tail = None
+        #         self.length = 0
+        #     elif self.head != self.tail:
+        #         node = self.head
+        #         nextNode = node.next
 
-            if node.value == val and node.next == None:
-                self.head = None
-                self.tail = None
-            elif node.value == val and node.next == self.tail:
-                self.head = node.next
-            else:
-                while node != None:
-                    if node.value == val:
-                        if prevNode == None:
-                            self.head = node.next
-                        else:
-                            prevNode.next = node.next
+        #         while nextNode != None:
+        #             if node.value == val and node == self.head:
+        #                 self.head = nextNode
+        #                 if node.next == self.tail:
+        #                     self.tail = nextNode
+        #                 return
+        #             elif nextNode.value == val and node == self.tail:
+        #                 self.tail = node
+        #                 return
+        #             elif nextNode.value == val and node != self.tail:
+        #                 node.next = nextNode.next
+        #                 return
+        #             node = nextNode
+        #             nextNode = nextNode.next
+        # else:
+        #     if self.head == None:
+        #         return
+
+        #     prevNode = None
+        #     node = self.head
+
+        #     if node.value == val and node.next == None:
+        #         self.head = None
+        #         self.tail = None
+        #     elif node.value == val and node.next == self.tail:
+        #         self.head = node.next
+        #     else:
+        #         while node != None:
+        #             if node.value == val:
+        #                 if prevNode == None:
+        #                     self.head = node.next
+        #                 else:
+        #                     prevNode.next = node.next
                             
-                        if node.next == None:
-                            self.tail = prevNode
-                    else:
-                        prevNode = node
-                    node = node.next
+        #                 if node.next == None:
+        #                     self.tail = prevNode
+        #             else:
+        #                 prevNode = node
+        #             node = node.next
             
     def clean(self):
         self.head = None
         self.tail = None
-        self.count = 0
+        self.length = 0
 
     def len(self):
-        return self.count
+        return self.length
 
     def insert(self, afterNode, newNode):
-        if afterNode == None:
-            if self.head == None:
-                self.head = newNode
-                self.tail = newNode
-            else:
-                currentHead = self.head
-                self.head = newNode
-                newNode.next = currentHead
-            self.count += 1
-        else:
-            node = self.head
-            while node != None:
-                if node == afterNode:
-                    if node == self.tail:
-                        node.next = newNode
-                        self.tail = newNode
-                        self.count += 1
-                        break
-                    else:
-                        currentNextNode = node.next
-                        node.next = newNode
-                        newNode.next = currentNextNode
-                        self.count += 1
-                        break
-                else:
-                    node = node.next
-                self.count += 1
+        if self.head is None:
+            self.head = self.tail = newNode
+            return
+        
+        currentNode = self.head
+
+        while currentNode != None:
+            if currentNode == afterNode:
+                if currentNode.next == None:
+                    self.tail = newNode
+                newNode.next = currentNode.next
+                currentNode.next = newNode
+                break
+            currentNode = currentNode.next
+
+
+        # if afterNode == None:
+        #     if self.head == None:
+        #         self.head = newNode
+        #         self.tail = newNode
+        #     else:
+        #         currentHead = self.head
+        #         self.head = newNode
+        #         newNode.next = currentHead
+        #     self.count += 1
+        # else:
+        #     node = self.head
+        #     while node != None:
+        #         if node == afterNode:
+        #             if node == self.tail:
+        #                 node.next = newNode
+        #                 self.tail = newNode
+        #                 self.count += 1
+        #                 break
+        #             else:
+        #                 currentNextNode = node.next
+        #                 node.next = newNode
+        #                 newNode.next = currentNextNode
+        #                 self.count += 1
+        #                 break
+        #         else:
+        #             node = node.next
+        #         self.count += 1
--- linked_list_test.py
+++ linked_list_test.py
@@ -3,13 +3,29 @@ from linked_list import Node
 from linked_list import LinkedList
 
 class LinkedListTest(unittest.TestCase):
-    def testDelete(self):
+    def testAddInTail(self):
+        node1 = Node(1)
+        node2 = Node(2)
+
+        list = LinkedList()
+
+        list.add_in_tail(node1)
+
+        self.assertEqual(list.head.value, 1)
+        self.assertEqual(list.tail.value, 1)
+        self.assertEqual(list.head.next, None)
+
+        list.add_in_tail(node2)
+
+        self.assertEqual(list.head.value, 1)
+        self.assertEqual(list.tail.value, 2)
+        self.assertEqual(list.head.next, node2)
+
+    def testDelete(self):        
+        list = LinkedList()
         node1 = Node(1)
         node2 = Node(2)
         node3 = Node(3)
-        
-        list = LinkedList()
-
         self.assertEqual(list.delete(1), None)
 
         list.add_in_tail(node1)
@@ -26,12 +42,12 @@ class LinkedListTest(unittest.TestCase):
 
         self.assertEqual(list.head, node1)
         self.assertEqual(list.tail, node3)
+        self.assertEqual(list.length, 2)
 
     def testClean(self):
         node1 = Node(1)
         node2 = Node(2)
         node3 = Node(3)
-
         list = LinkedList()
 
         list.add_in_tail(node1)
@@ -42,13 +58,13 @@ class LinkedListTest(unittest.TestCase):
 
         self.assertEqual(list.head, None)
         self.assertEqual(list.tail, None)
+        self.assertEqual(list.length, 0)
 
     def testLen(self):
+        list = LinkedList()
         node1 = Node(1)
         node2 = Node(2)
         node3 = Node(3)
-
-        list = LinkedList()
 
         self.assertEqual(list.len(), 0)
 
@@ -59,32 +75,31 @@ class LinkedListTest(unittest.TestCase):
 
     def testInsert(self):
         list = LinkedList()
-        list.insert(None, Node(10))
-
         node1 = Node(1)
         node2 = Node(2)
         node3 = Node(3)
+        node4 = Node(10)
         
         list.add_in_tail(node1)
         list.add_in_tail(node2)
         list.add_in_tail(node3)
         
-        self.assertEqual(list.head.value, 10)
+        self.assertEqual(list.head.value, 1)
 
-        node4 = Node(15)
         list.insert(node2, node4)
-        
-        self.assertEqual(list.find(15), node4)
+        node = list.find(10)
+        self.assertEqual(node.value, 10)
+        self.assertEqual(node.next, node3)
 
-    def testFindAll(self):
+    def testFindAll(self):      
+        list = LinkedList()
+
+        self.assertEqual(list.find_all(2), [])
+
         node1 = Node(1)
         node2 = Node(2)
         node3 = Node(3)
         node4 = Node(2)
-        
-        list = LinkedList()
-
-        self.assertEqual(list.find_all(2), [])
 
         list.add_in_tail(node1)
         list.add_in_tail(node2)
@@ -92,7 +107,6 @@ class LinkedListTest(unittest.TestCase):
         list.add_in_tail(node4)
 
         self.assertEqual(list.find_all(2), [node2, node4])
+
 if __name__ == "__main__":
     unittest.main()
-
-
