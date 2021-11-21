edb5e83f3aed0be48c802c9bb707d90e873df53b
Смирнов Александр Андреевич <aasmi124@mts.ru>
2021-11-21 21:48:52 +0300

    finish task

---
+++ hash.py
@@ -0,0 +1,57 @@
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        length = 0
+        for x in range(0, len(value), 1):
+            length += ord(value[x])
+
+        return length % self.size
+
+    def seek_slot(self, value):
+        currentIndex = self.hash_fun(value)
+
+        if self.slots[currentIndex] is not None:
+            x = currentIndex + 1
+
+            while x != currentIndex:
+                if self.slots[x] is None:
+                    return x
+                elif x == currentIndex - 1:
+                    return None
+                elif x == len(self.slots):
+                    x = 0
+                else:
+                    x += 1
+
+        return currentIndex
+
+    def put(self, value):
+        availableSlot = self.seek_slot(value)
+
+        if availableSlot is not None:
+            self.slots[availableSlot] = value
+            return availableSlot
+        
+        return None
+
+    def find(self, value):
+        currentIndex = self.hash_fun(value)
+
+        if self.slots[currentIndex] != value:
+             x = currentIndex + 1
+
+             while x != currentIndex:
+                if self.slots[x] == value:
+                    return x
+                elif x == currentIndex - 1:
+                    return None
+                elif x == len(self.slots):
+                    x = 0
+                else:
+                    x += 1
+        
+        return currentIndex
---
+++ hash_test.py
@@ -0,0 +1,39 @@
+import unittest
+from hash import HashTable
+
+class HashTest(unittest.TestCase):
+    def testHashFunc(self):
+        ht = HashTable(19, 3)
+
+        hash1 = ht.hash_fun('строка1')
+
+        self.assertGreaterEqual(hash1, -1)
+        self.assertLessEqual(hash1, ht.size)
+
+        hash2 = ht.hash_fun('Строка2')
+
+        self.assertGreaterEqual(hash2, -1)
+        self.assertLessEqual(hash2, ht.size)
+    
+    def testSeekSlot(self):
+        ht = HashTable(19, 3)
+
+        self.assertEqual(ht.seek_slot('строка1'), 1)
+        self.assertEqual(ht.put('строка1'), 1)
+
+        self.assertEqual(ht.put('строка1'), 2)
+
+    def testPut(self):
+        ht = HashTable(19, 3)
+
+        self.assertEqual(ht.put('строка1'), 1)
+
+    def testFind(self):
+        ht = HashTable(19, 3)
+
+        ht.put('строка1')
+
+        self.assertEqual(ht.find('строка1'), 1)    
+
+if __name__ == "__main__":
+    unittest.main()
