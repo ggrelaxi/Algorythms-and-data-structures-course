b023b9f06a2e695635e162261f664ae54ea94b74
aleksandr.smirnov <aleksandr.smirnov@stm-labs.ru>
2022-07-29 21:43:49 +0300

    refactor zero degree

--- get_degree.py
+++ get_degree.py
@@ -1,4 +1,6 @@
 def get_degree(base, degree):
+    if degree == 0:
+        return 1
     if degree == 1:
         return base * degree
     return base * get_degree(base, degree - 1)
--- get_degree_test.py
+++ get_degree_test.py
@@ -7,6 +7,19 @@ class GetDegreeTest(unittest.TestCase):
         self.assertEqual(get_degree(2, 2), 4)
         self.assertEqual(get_degree(2, 3), 8)
         self.assertEqual(get_degree(2, 10), 1024)
+        self.assertEqual(get_degree(10, 0), 1)
+        self.assertEqual(get_degree(2, 0), 1)
+
+    def testGetBaseZeroDegree(self):
+        self.assertEqual(get_degree(0, 1), 0)
+        self.assertEqual(get_degree(0, 10), 0)
+        self.assertEqual(get_degree(0, 0), 1)
+    
+    def testNegativeBaseDegree(self):
+        self.assertEqual(get_degree(-2, 2), 4)
+        self.assertEqual(get_degree(-2, 3), -8)
+
+    
 
 
 if __name__ == "__main__":
