315913ede0d537effafef304e77f7ccc298b8d27
aleksandr.smirnov <aleksandr.smirnov@stm-labs.ru>
2022-08-02 10:21:58 +0300

    finish find second max task

---
+++ get_second_max.py
@@ -0,0 +1,18 @@
+def get_second_max(list):
+    if len(list) < 2:
+        return 0
+
+    def inner_f(list, index, first_max, second_max):
+        if index > len(list) - 1:
+            return second_max
+        current = list[index]
+
+        if current > first_max:
+            second_max = first_max
+            first_max = current
+        elif current > second_max and current < first_max:
+            second_max = current
+
+        return inner_f(list, index + 1, first_max, second_max)
+
+    return inner_f(list, 0, 0, 0)
