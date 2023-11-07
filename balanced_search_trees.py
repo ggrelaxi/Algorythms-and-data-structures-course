00e79dd768d84fcacd411676392ae30c92425773
aleksandr.smirnov <ggrelaxi@gmail.com>
2023-11-07 13:49:09 +0300

    finish create generateBBSTArray

---
+++ balanced_search_trees.py
@@ -0,0 +1,25 @@
+
+def GenerateBBSTArray(a):
+    a.sort()
+    bbst = [None] * len(a)
+
+    def iter(part, acc, index=0):
+        if len(part) == 0:
+            return
+
+        leftIndex = (2 * index) + 1
+        rightIndex = (2 * index) + 2
+
+        middleIndex = len(part) // 2
+        currentItem = part[middleIndex]
+        acc[index] = currentItem
+
+        iter(part[:middleIndex], acc, leftIndex)
+        iter(part[middleIndex + 1:], acc, rightIndex)
+
+    iter(a, bbst)
+
+    return bbst
+
+
+GenerateBBSTArray([1, 3, 5])
